0c7f27b95f0504b49ad94a8f3ded1bd2e358695a
e-aub <eaub@proton.me>
2024-09-30 18:14:00 +0100

    dog years

---
+++ dog-years.js
@@ -0,0 +1,14 @@
+const planets = {earth : 1.0,
+    mercury : 0.2408467,
+    venus : 0.61519726,
+    mars :1.8808158,
+    jupiter : 11.862615,
+    saturn :29.447498 ,
+    uranus : 84.016846,
+    neptune : 164.79132};
+
+const dogYears = (planet, dogsAgeInSeconds) => {
+    dogsAgeInYears = dogsAgeInSeconds/ 31_557_600
+    dogsAgeInYears = 7 * dogsAgeInYears *(planets['earth']/planets[planet])
+    return Math.round(dogsAgeInYears*100) / 100
+}
