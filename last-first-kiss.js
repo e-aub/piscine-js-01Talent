141c4a957e15b8c8b4d9f92d5045f1cc9ff9b951
e-aub <eaub@proton.me>
2024-09-30 17:08:32 +0100

    remove out of range protection

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,18 +1,12 @@
 const first = x => {
-    if (x.length > 0) {
       return x[0];
-    }
   };
   
   const last = x => {
-    if (x.length > 0) {
       return x[x.length - 1];
-    }
   };
   
   const kiss = x => {
-    if (x.length >= 2) {
       return [x[x.length-1], x[0]];
-    }
   };
   
