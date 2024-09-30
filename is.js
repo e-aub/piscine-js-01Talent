a31d85c9532a9063aaac4208212f07e2b3770234
e-aub <eaub@proton.me>
2024-09-30 20:26:24 +0100

    edited is obj

--- is.js
+++ is.js
@@ -5,13 +5,7 @@ is.bool = x => typeof x === 'boolean'
 is.undef = x => typeof x === 'undefined' 
 is.def = x => typeof x !== 'undefined'
 is.arr = x => Array.isArray(x)
-is.obj = x => {
-    const typeOfX = typeof x;
-    if (typeOfX === 'null'|| typeOfX === 'object'){
-        return true
-    }
-    return false
-}
+is.obj = x => typeof x === 'object' && !Array.isArray(x) && x != null
 is.fun = x => typeof x ==='function'
 is.truthy = x => Boolean(x) === true
 is.falsy = x => Boolean(x) === false
