5a4e83193307f6af80bf7937314d5c4d1050f317
e-aub <eaub@proton.me>
2024-09-30 17:15:54 +0100

    concat str

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,12 +1,1 @@
-const first = x => {
-      return x[0];
-  };
-  
-  const last = x => {
-      return x[x.length - 1];
-  };
-  
-  const kiss = x => {
-      return [x[x.length-1], x[0]];
-  };
-  
+const concatStr =(x, y) => x+y
