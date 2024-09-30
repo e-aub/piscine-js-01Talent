782329559867777f7c6a709a7d0268efb18468cd
e-aub <eaub@proton.me>
2024-09-30 17:17:04 +0100

    reset to last-first-kiss

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,1 +1,12 @@
-const concatStr =(x, y) => x+y
+const first = x => {
+      return x[0];
+  };
+  
+  const last = x => {
+      return x[x.length - 1];
+  };
+  
+  const kiss = x => {
+      return [x[x.length-1], x[0]];
+  };
+  
