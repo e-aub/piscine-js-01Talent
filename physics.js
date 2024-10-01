9ebdb314ffbba011d9f49629a2aad203011977c5
aelhadda <eaub@proton.me>
2024-10-01 11:54:25 +0100

    fix undefined d

--- physics.js
+++ physics.js
@@ -1,12 +1,13 @@
-const isNotUndefinedOrNull = (x) =>  x != null
+const isDefined = (x) =>  x != null
 const isNotZero = (x) => x != '0'
 const getAcceleration = function(object) {
-    if (isNotUndefinedOrNull(object.f) && isNotUndefinedOrNull(object.m) && isNotZero(object.m)){
+    if (isDefined(object.f) && isDefined(object.m) && isNotZero(object.m)){
         return object.f/object.m
-    }else if (isNotUndefinedOrNull(object.Δv) && isNotUndefinedOrNull(object.Δt) && isNotZero(object.Δt)){
+    }else if (isDefined(object.Δv) && isDefined(object.Δt) && isNotZero(object.Δt)){
         return object.Δv/ object.Δt
-    }else if (isNotUndefinedOrNull(object.d) && isNotUndefinedOrNull(object.t) && isNotZero(object.t)){
-        return (2 * d) / (t *t)
+    }else if (isDefined(object.d) && isDefined(object.t) && isNotZero(object.t)){
+        return (2 * object.d) / (object.t *object.t)
     }
     return "impossible"
 }
+getAcceleration({ d: 10, t: 2, Δv: 100 })
