dbcbdc1fc447f2309bf80e497edad631f5ec479b
e-aub <eaub@proton.me>
2024-10-02 21:38:35 +0100

    convert to negative using bitwise

--- elementary.js
+++ elementary.js
@@ -1,28 +1,55 @@
 const divide = function(a, b){
-    if (b > 0){
+    var sign = 1
+    if (a < 0||b < 0){
+        sign = -1
+    }
+
+    a = Math.abs(a)
+    b = Math.abs(b)
+    
+    if (b != 0){
         var quotient = 0
         while(a >= b){
             a -= b
             quotient += 1
         }
-        return quotient
+        return quotient * sign
     }
-    return undefined
+    return Infinity * sign
 }
 
 
 const modulo = function(a, b){
+    var sign = 1
+    if (a < 0){
+        sign = -1
+    }
+
+    a = Math.abs(a)
+    b = Math.abs(b)
     while(a >= b){
         a -= b
-    }
-    return a
+     }
+    return sign === -1 ? ~a +1 : a 
 }
+
 
 const multiply = function(a, b){
     var result = 0
+    var sign = 1
+    if (a == 0 || b == 0){
+        return 0
+    }else if (a < 0||b < 0){
+        sign = -1
+        if (a < 0){
+            a = Math.abs(a)
+        }else if (b < 0){
+            b = Math.abs(b)
+        }
+    }
     while(b > 0){
         result += a
         b--
     }
-    return result
+    return sign === -1 ? ~result + 1 : result
 }
