192389af05fcee336a6600eaa95c4f4f0dab344b
e-aub <eaub@proton.me>
2024-10-02 21:57:00 +0100

    fix multiply sign when both are negative

--- elementary.js
+++ elementary.js
@@ -44,14 +44,20 @@ const multiply = function(a, b){
     var positive = true
     if (a == 0 || b == 0){
         return 0
-    }else if (a < 0||b < 0){
+    }
+    if (a < 0){
         positive = false
-        if (a < 0){
-            a = Math.abs(a)
-        }else if (b < 0){
-            b = Math.abs(b)
+    } 
+    if (b < 0){
+        if (!positive){
+            positive = true
+        }else{
+            positive = false
         }
     }
+    a = Math.abs(a)
+    b = Math.abs(b)
+
     while(b > 0){
         result += a
         b--
