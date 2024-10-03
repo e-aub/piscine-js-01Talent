494a60b90b19de7e8106996f5fb87ab39bc7af95
e-aub <eaub@proton.me>
2024-10-03 15:34:48 +0100

    kcut

---
+++ keep-cut.js
@@ -0,0 +1,34 @@
+//: returns the string with the first 2 characters removed.
+const cutFirst = function(str){
+    return str.slice(2)
+}
+
+//returns the string with the last 2 characters removed.
+const cutLast = function(str){
+    return str.slice(0, -2)
+}
+
+//returns the string with the first 2 characters and the last 2 characters removed.
+
+const cutFirstLast = function(str){
+    return str.slice(2, -2)
+}
+
+//returns only the first 2 characters.
+
+const keepFirst = function(str){
+    return str.slice(0, 2)
+}
+
+
+//returns only the last 2 characters.
+
+const keepLast = function(str){
+    return str.slice(-2)
+}
+
+//returns the first 2 characters, and the last 2 characters.
+
+const keepFirstLast = function(str){
+    return keepFirst(str) + keepLast(str)
+}
