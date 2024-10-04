4a148d326c31e95016f6d0f57e3f12676c272aed
e-aub <eaub@proton.me>
2024-10-04 05:11:21 +0100

    fix accessing to and undefined property

--- get.js
+++ get.js
@@ -2,7 +2,11 @@ const get= function(src, path){
     path = path.split('.')
     console.log(path)
     for(let i = 0; i < path.length;i++){
+        if(src != undefined){
         src = src[path[i]]
+        }else{
+            break
+        }
     }
     return src
 }
