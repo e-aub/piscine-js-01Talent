0b730832d2fa8548a7b4bb0824f98e1c14d228f5
e-aub <eaub@proton.me>
2024-10-04 01:08:57 +0100

    set is string to true if it is a string

--- reverser.js
+++ reverser.js
@@ -2,6 +2,7 @@ const reverse = function(arg1){
     var isString = false
     var result = []
     if(typeof arg1 === 'string'){
+        isString = true
         arg1 = arg1.split('')
     }
     for(let i = arg1.length-1; i >= 0; i--){
