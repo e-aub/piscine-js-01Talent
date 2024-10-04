a9290aefd2a2dccc09da148a4500715f10881ac4
e-aub <eaub@proton.me>
2024-10-04 22:03:06 +0100

    fix if match is in the end

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,6 +1,10 @@
 const letterSpaceNumber = function(str){
-    const regex = new RegExp(`([a-zA-Z] \\d)[^a-zA-z0-9]`, 'g') 
+    const regex = new RegExp(`([a-zA-Z] \\d)([^a-zA-z0-9]|$)`, 'g') 
     var matches =  [...str.matchAll(regex)]
     const result = matches.map(match => match[1])
     return result
 }
+
+
+console.log(letterSpaceNumber("It's 20 past 3"))
+// output: ['e 1']
