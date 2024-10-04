c972dc51330483011a8caff27cd2dafc0583150f
e-aub <eaub@proton.me>
2024-10-04 01:07:24 +0100

    reverser

---
+++ reverser.js
@@ -0,0 +1,11 @@
+const reverse = function(arg1){
+    var isString = false
+    var result = []
+    if(typeof arg1 === 'string'){
+        arg1 = arg1.split('')
+    }
+    for(let i = arg1.length-1; i >= 0; i--){
+        result.push(arg1[i])
+    }
+    return isString ? result.join('') : result 
+}
