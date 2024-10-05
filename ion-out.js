8d6e7444b79a5fdca35398d246c70daf479016ec
e-aub <eaub@proton.me>
2024-10-05 01:15:27 +0100

    fix regex to match t befor ion

--- ion-out.js
+++ ion-out.js
@@ -1,9 +1,10 @@
 const ionOut = function(str){
-    const regex = /([\w]*t[\w]*)(ion)/
+    const regex = /([\wt]+)(ion)/
     str = str.split(/[ ,.!?:;]/)
     var result = new Array
     for(let i = 0; i < str.length;i++){
         var  match = str[i].match(regex)
+        console.log(match)
         if (match !== null){
             result.push(match[1])
         } 
