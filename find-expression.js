48db33970e223e6b4a41cfcd6f6eb01f3fc9dd24
e-aub <eaub@proton.me>
2024-10-05 08:40:44 +0100

    edit while condition

--- find-expression.js
+++ find-expression.js
@@ -1,13 +1,8 @@
-const add4 = '+4'
-const mul2 = '*2'
 const findExpression = function(number){
     if (number % 2 !== 0) return undefined;
     var n = 1
     var res = '1'
-    while(n <= number){
-        if(n == number){
-            return res
-        }
+    while(n < number){
         if(n*2 < number){
             n*=2
             res += ' ' + mul2
@@ -18,5 +13,3 @@ const findExpression = function(number){
     }
     return res
 }
-
-console.log(findExpression(280))
