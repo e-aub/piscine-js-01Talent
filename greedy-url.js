adb3d9a9f65f1c74934b13a9bf5623af0d38d6f1
e-aub <eaub@proton.me>
2024-10-05 07:28:49 +0100

    fix regex

--- greedy-url.js
+++ greedy-url.js
@@ -1,5 +1,5 @@
 const getURL = function (dataSet) {
-    const urlRegex = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?(?:[-a-zA-Z0-9()\[\],@:%_\+.~#?&\/=]*)/g;
+    const urlRegex = /https?:\/\/(?:www\.)?[\w.-]{1,256}(\.[a-zA-Z]{1,6})(?:[\/?&][\w@:%._~#=-]*)*/g;
     return dataSet.match(urlRegex) || []; // Return an empty array if no matches
 }
 
