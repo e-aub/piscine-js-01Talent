c9cf9970fb4d2c83c864b45e35c73d6c946e73ed
e-aub <eaub@proton.me>
2024-10-05 23:44:15 +0100

    valid ip

---
+++ valid-ip.js
@@ -0,0 +1,5 @@
+const findIP = function(adresses){
+    const regex = /((?<!\d)(([1-9][0-9]?)|(1[0-9][0-9]|2[0-4][0-9]|25[0-5])|0)\.){3}(([1-9][0-9]?)([0-9]{0,2})|0?)(:([0-5]?\d{0,4}|[1-5]\d{5}|6[0-4]\d{3}|65[0-3]\d{2}|65[4-5][0-5]))?(?!\d)/g
+    const validIPs = dataSet.match(regex);
+    return [...new Set(validIPs)];
+}
