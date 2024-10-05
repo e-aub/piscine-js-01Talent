f38397d696ac1666d7e08c672e5895c65be841d1
e-aub <eaub@proton.me>
2024-10-05 02:05:23 +0100

    fix t at beginning

--- ion-out.js
+++ ion-out.js
@@ -1,11 +1,11 @@
 const ionOut = function(str){
-    const regex = /([\wt]+)(ion)/
+    const regex = /tion/
     str = str.split(/[ ,.!?:;]/)
     var result = new Array
     for(let i = 0; i < str.length;i++){
         var  match = str[i].match(regex)
         if (match !== null){
-            result.push(match[1])
+            result.push(str[i].slice(0, -3))
         } 
     }
     return result
