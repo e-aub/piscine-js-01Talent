8f46985437e778d174dd6453d473b79c1ac6a007
e-aub <eaub@proton.me>
2024-10-05 07:01:32 +0100

    greedy

--- greedy-url.js
+++ greedy-url.js
@@ -1,6 +1,6 @@
 
 const getURL = function (dataSet) {
-    const urlRegex = /(?!.*-{2,})(?=.{1,253}$)(http(?:s){0,1}:\/\/)?(?:(www\.)?)(?!-)((?:[A-Za-z0-9-]{1,63}(?<!-)\.)+(?:(?!-)[A-Za-z]{2,}))([\/\S]*)/g;
+    const urlRegex = /https?:\/\/(?:www\.)?[a-zA-Z0-9@:%._\+=~#-]{1,256}(\.[a-zA-Z0-9()]{1,6})?(?:[-A-Za-z0-9()\[\]%,:_@\+\/.~#?=&]*)/g;
     var matches = dataSet.matchAll(urlRegex);
     return [...matches].map((match => match[0]))
     
@@ -8,7 +8,7 @@ const getURL = function (dataSet) {
 
 const greedyQuery = function(dataSet){
    var urls = getURL(dataSet);
-   var parameter = new RegExp(/(\?(?!.*\?+)(?:[^=]+=(?:[^&]*))){1}((?:(&[^=-]+=*[^&]*){2}))/g)
+   var parameter = new RegExp(/\?([^&=]+=[^&]*){1}((\&([^&=]+=[^&]*))|(\&([^&=]+))){1}/g)
    var result = new Array
     for(let i = 0; i < urls.length;i++){
         if(parameter.test(urls[i])){
@@ -20,7 +20,7 @@ const greedyQuery = function(dataSet){
 
 const notSoGreedy = function(dataSet){
     var urls = getURL(dataSet);
-    var parameter = new RegExp(/(\?(?!.*\?+)(?:[^=]+=(?:[^&]*))){1}((?:(&[^=-]+=*[^&]*){1,2}))$/)
+    var parameter = new RegExp(/\?([^&=]+=[^&]*){1}((\&([^&=]+=[^&]*))|(\&([^&=]+))){1,2}$/g)
     var result = new Array
      for(let i = 0; i < urls.length;i++){
          if(parameter.test(urls[i])){
