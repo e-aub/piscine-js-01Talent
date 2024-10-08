ff1fe88cbf2e3574d976db48eaea92f59ac60ddf
e-aub <eaub@proton.me>
2024-10-08 07:00:32 +0100

    is past

---
+++ date-is.js
@@ -0,0 +1,42 @@
+const isValid = function(date){
+    date = new Date(date);
+    if(!Number.isNaN(date)){
+        return true
+    }
+    return false
+}
+
+const isAfter = function(date1, date2){
+    if(date1 > date2){
+        return true
+    }
+    return false
+}
+
+const isBefore = function(date1, date2){
+    if(date1 < date2){
+        return true
+    }
+    return false
+}
+const isFuture = function(date){
+    if(isValid(date)){
+        var now = Date.now()
+        time = new Date(date)
+        if(time > now){
+            return true
+        }
+    }
+    return false
+}
+
+const isPast = function(date){
+    if(isValid(date)){
+        var now = Date.now()
+        time = new Date(date)
+        if(time < now){
+            return true
+        }
+    }
+    return false
+}
