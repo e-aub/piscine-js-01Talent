06ec01aedd4e6227b9502ea053365744f5fbe224
e-aub <eaub@proton.me>
2024-10-08 12:34:13 +0100

    number time in ms

--- date-is.js
+++ date-is.js
@@ -2,11 +2,12 @@ const isValid = function (date) {
     if (new Date(date).toString() === 'Invalid Date') {
       return false;
     }
-    if (!(date instanceof Date) && !Number.isNaN(date)) {
+    if (!(date instanceof Date) && typeof date !== 'number') {
       return false;
     }
     return true;
   };
+  
 const isAfter = function(date1, date2){
     if(date1 > date2){
         return true
