ac35689bb25752b6290dace613901b17cf4a7777
aelhadda <eaub@proton.me>
2024-10-09 19:21:50 +0100

    test

---
+++ using-map.js
@@ -0,0 +1,29 @@
+const citiesOnly = function (array) {
+    return array.map(item => item.city)
+}
+
+const upperCasingStates = function (array) {
+    return array.map(item => item.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
+}
+
+const fahrenheitToCelsius = function (FTempratures) {
+    return FTempratures.map(ftemp => {
+        const numeric = parseFloat((ftemp.replace(/\s+/g, '').slice(0, ftemp.length - 2)))
+        return Math.round((numeric - 32) * 5 / 9) + '°C'
+    })
+
+}
+
+const trimTemp = function (array) {
+    return array.map(obj => {
+        obj.temperature = obj.temperature.replace(/\s+/g, '');
+        return obj
+    })
+}
+
+
+const tempForecasts = function (array) {
+    return array.map(obj => {
+        return `${fahrenheitToCelsius([obj.temperature])[0]}elsius in ${obj.city}, ${upperCasingStates([obj.state])[0]}`
+    })
+}
