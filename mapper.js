41c251d66c3ed8c19662973608eca79c4231e9df
aelhadda <eaub@proton.me>
2024-10-09 23:55:10 +0100

    mappeer

---
+++ mapper.js
@@ -0,0 +1,17 @@
+const map = function(array, func){
+    var result = []
+    for(let i = 0; i < array.length; i++){
+        result.push(func(array[i], i, array))
+    }
+    return result
+}
+
+
+const flatMap = function(array, func){
+    var mapped = map(array, func)
+    var result = []
+    for(let element of mapped){
+        result.concat(element)
+    } 
+    return result
+}
