41874c989bddc6d2849527c9b0e109412f8b6037
aelhadda <eaub@proton.me>
2024-10-09 01:02:23 +0100

    finnaly

--- get-some-time.js
+++ get-some-time.js
@@ -1,76 +1,13 @@
-function getDaysInMonth(year, month) {
-    const date = new Date(year, month + 1, 0);
-    date.setFullYear(year, month + 1, 0); // Use setFullYear for years < 1000
-
-    return date.getDate()
+function firstDayWeek(week, year) {
+  let start = new Date(year);
+  let current = new Date(start.getTime() + (week - 1) * 7 * 24 * 60 * 60 * 1000);
+  if (week === 1) {
+    return `01-01-${year}`
   }
-
-  function getDayNumber(day, month, year) {
-    const date = new Date(year, month - 1, day); // JavaScript months are 0-indexed
-    const dayName = date.toLocaleString('en-US', { weekday: 'long' });
-  
-    const daysMap = {
-      'Monday': 1,
-      'Tuesday': 2,
-      'Wednesday': 3,
-      'Thursday': 4,
-      'Friday': 5,
-      'Saturday': 6,
-      'Sunday': 7
-    };
-  
-    return daysMap[dayName];
+  while (current.getDay() !== 1) {
+    count++
+    current.setDate(current.getDate() - 1);
   }
-  
- 
-  const firstDayWeek = function (week, year) {
-   let alldays=(week*7)-6
-    let days =(week*7)-6
-    let i = 0;
-    let count=0
-    while (days - getDaysInMonth(year, i) > 0) {
-        count+=getDaysInMonth(year,i)
-        days=days - getDaysInMonth(year, i)
-        i++;
-     }
-    //   console.log("days",alldays-count-getDayNumber(alldays-count,i+1,year)+1)
-    //   console.log(i+1)
-    //   console.log(getDayNumber(alldays-count,i+1,year))
-    // console.log("ds",getDayNumber(alldays-count,i+1,year))
-    // console.log("count",count)
-    // console.log("aldays",alldays)
-   
-    if ( (1+alldays-count-getDayNumber(alldays-count,i+1,year))<=0){
-        if (year<=999){return format(alldays) +"-"+format((i+1))+"-"+year}
+  return `${String(current.getDate()).padStart(2, '0')}-${String(current.getMonth() + 1).padStart(2, '0')}-${String(current.getFullYear()).padStart(4, '0')}`
 
- 
-     return   format(alldays)+"-"+format(i+1)+"-"+year
-    }
-
-    let tsltdays=  1+alldays-count-getDayNumber(alldays-count,i+1,year)
-    let rsltmonth=i+1
-    let rsltteas=year
-
- 
-    
-
-if (year<=999){return format(tsltdays+1) +"-"+format((rsltmonth))+"-"+rsltteas}
-
- 
-     return   format(tsltdays)+"-"+format(rsltmonth)+"-"+rsltteas
-
-  
-         
-  };
-//    console.log(firstDayWeek(2, "2017"));
-//    console.log(format(3));
-
-  
-function format(nbr){
-    if (nbr<10){
-        nbr="0"+nbr
-        
-    }
-    return nbr
 }
-
