94b8e815e88301b0febf2dc068db24554b26b38a
aelhadda <eaub@proton.me>
2024-10-09 22:23:28 +0100

    leng words

---
+++ long-words.js
@@ -0,0 +1,15 @@
+const longWords = function(array){
+    return array.every(str => str.length >= 5)
+}
+
+
+const oneLongWord = function(array){
+    return array.some(str => str.length >= 10)
+}
+
+const noLongWords = function(array){
+    return array.every(str => !str.length >= 7)
+}
+
+
+// console.log(longWords(['aaaaaaaaaa','aaaa','aaaddddddddd']))
