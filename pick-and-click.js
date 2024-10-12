d816ecc2657c95ea2c62011052e5d41e8cbea306
aelhadda <eaub@proton.me>
2024-10-12 02:31:41 +0100

    fix hue and lum values in corner

--- pick-and-click.js
+++ pick-and-click.js
@@ -35,15 +35,15 @@ function pick() {
         y = event.clientY;
 
 
-        xLine.setAttribute('x1', 0);
-        xLine.setAttribute('y1', y);
-        xLine.setAttribute('x2', width);
-        xLine.setAttribute('y2', y);
+        xLine.setAttribute('x1', `${0}`);
+        xLine.setAttribute('y1', `${y}`);
+        xLine.setAttribute('x2', `${width}`);
+        xLine.setAttribute('y2', `${y}`);
 
-        yLine.setAttribute('x1', x);
-        yLine.setAttribute('y1', 0)
-        yLine.setAttribute("x2", x);
-        yLine.setAttribute("y2", height);
+        yLine.setAttribute('x1', `${x}`);
+        yLine.setAttribute('y1', `${0}`)
+        yLine.setAttribute("x2", `${x}`);
+        yLine.setAttribute("y2", `${height}`);
 
         const hsl = calculateHsl(x, y, height, width)
 
@@ -57,6 +57,7 @@ function pick() {
     })
 
     document.addEventListener('click', (event) => {
+        event.preventDefault();
         document.execCommand('copy');
     })
 
