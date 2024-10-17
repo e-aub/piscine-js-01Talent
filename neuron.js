096b428511116f4dd3a41bb3772f7b3a153d75e6
e-aub <eaub@proton.me>
2024-10-17 07:55:00 +0100

    replace turncate with if conditions to make code more readable

--- neuron.js
+++ neuron.js
@@ -5,16 +5,22 @@ function neuron(arr) {
     const questionRegex = /(?<key>.+?):\s+(?<question>.+?)\s+-\s+Response:\s*(?<response>.+)/i;
     var result = {};
     for (let str of arr) {
-        let questionMatch = questionRegex.exec(str);
+        const questionMatch = questionRegex.exec(str);
         if (questionMatch) {
-            result[questionMatch.groups.key] ? '' :result[questionMatch.groups.key] = {}
-            const snakeCaseQuestion = toSnakeCase(questionMatch.groups.question)
-            result[questionMatch.groups.key][snakeCaseQuestion]
-                ? result[questionMatch.groups.key][snakeCaseQuestion]['responses'].push(questionMatch.groups.response)
-                : result[questionMatch.groups.key][snakeCaseQuestion] = {
-                    [questionMatch.groups.key.toLocaleLowerCase().slice(0,questionMatch.groups.key.length-1)] : questionMatch.groups.question, 'responses': [questionMatch.groups.response]
-                }
+            const key = questionMatch.groups.key
+            if (!result[key]) {
+                result[key] = {};
             }
+            const snakeCaseQuestion = toSnakeCase(questionMatch.groups.question)
+            if (result[key][snakeCaseQuestion]) {
+                result[key][snakeCaseQuestion]['responses'].push(questionMatch.groups.response);
+            } else {
+                result[key][snakeCaseQuestion] = {
+                    [key.toLowerCase().slice(0, key.length - 1)]: questionMatch.groups.question,
+                    'responses': [questionMatch.groups.response]
+                };
+            }
+        }
     }
     return result
 }
