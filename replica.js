311fd833223a5fb7835eb00585962de9608bd811
e-aub <eaub@proton.me>
2024-10-17 02:07:13 +0100

    merge if both are object otherwise assign value to that key

--- replica.js
+++ replica.js
@@ -1,26 +1,21 @@
 function replica(...objects) {
-    const result = {}
+    const result = {};
+
     for (let obj of objects) {
         if (obj === null || typeof obj !== 'object') {
-            continue
+            continue;
         }
         for (let [key, value] of Object.entries(obj)) {
-            if (typeof value === 'object' && typeof obj !== 'function' && !obj instanceof RegExp) {
-                result[key] = result[key] || {}
-                result[key] = replica(result[key], value)
+            if (typeof value === 'object' && value !== null && !Array.isArray(value) && !(value instanceof RegExp)) {
+                result[key] = result[key] || {};
+                result[key] = replica(result[key], value);
             } else {
-                if (typeof result[key] === 'object') {
-                    result[key] = { ...result[key], ...value }
-                }else{
-                    result[key] = value
-                }
+                result[key] = value;
             }
-
         }
     }
-
-    return result
-
+    return result;
 }
 
-// console.log(replica({ line: 'Replicants are like any other machine' }, { author: 'Rich' }))
+// console.log(replica({ line: 'Replicants are like any other machine' }, { author: 'Rich' }));
+// console.log(replica({ a: { b: [2] } }, { a: [4] }).a);
