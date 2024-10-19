77bb2195573828097387decd0af4fc9288fd31c2
e-aub <eaub@proton.me>
2024-10-19 03:58:06 +0100

    test

--- throttle.js
+++ throttle.js
@@ -1,34 +1,30 @@
-function throttle(func, wait){
+function throttle1(func, wait) {
     let waitingArgs = null
     let souldWait = false
 
-    return function(...args){
-        if(souldWait){
+    return function (...args) {
+        if (souldWait) {
             waitingArgs = args
             return
-        }else{
-            if(waitingArgs == null){
-                func(...args)
-                souldWait = true
-            }else{
-                func(...waitingArgs)
-                waitingArgs = null
-                souldWait = true
-            }
-            setTimeout(()=>{
+        } else {
+            func(...args)
+            souldWait = true
+            setTimeout(() => {
                 souldWait = false
+                if(waitingArgs !== null){
+                    func(...waitingArgs)
+                    waitingArgs = null
+                }
             }, wait)
         }
     }
-
-
 }
 
 
-// document.getElementById('input').addEventListener('input',e=>{
+// document.getElementById('input').addEventListener('input', e => {
 //     throtteled(e.target.value)
 // })
 // const output = document.getElementById('output')
-// const throtteled = throttle((value)=>{
+// const throtteled = throttle1((value) => {
 //     output.innerText = value
 // }, 2000)
