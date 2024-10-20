8308677e7a4cc9f778205268313a69dce09d556e
aelhadda <eaub@proton.me>
2024-10-20 04:04:59 +0100

    add error handling

--- all.js
+++ all.js
@@ -1,8 +1,13 @@
-async function all(promises){
-    promises = {...promises}
+async function all(promises) {
+    promises = { ...promises }
     const entries = Object.entries(promises)
-    for(let entry of entries){
-        promises[entry[0]] = await entry[1]
+    for (let entry of entries) {
+        try {
+            const resp = await entry[1]
+            promises[entry[0]] = resp
+        } catch (error) {
+            throw error
+        }
     }
     return promises
 }
