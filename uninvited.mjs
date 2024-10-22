5843337b906669d108c8f08e89a4d4c7b0734a41
aelhadda <eaub@proton.me>
2024-10-22 16:41:49 +0100

    uninvited test

---
+++ uninvited.mjs
@@ -0,0 +1,23 @@
+import http from 'http'
+import {writeFileSync } from 'fs'
+
+http.createServer((req, res) => {
+    let recivedData = ''
+    req.on('data', dataChunk => {
+        recivedData += dataChunk.toString()
+    })
+    let fileName = req.url.replace('/', '') + '.json'
+    req.on('end', () => {
+        try {
+            writeFileSync(fileName, recivedData);
+            res.setHeader('content-type', 'application/json')
+            res.statusCode = 201
+            res.end(recivedData)
+        } catch (error) {
+            console.log(error)
+            res.setHeader('content-type', 'application/json')
+            res.statusCode = 500
+            res.end(JSON.stringify({ error: 'server failed' }))
+        }
+    })
+}).listen(5000, 'localhost', () => console.log('starting server at port :5000'))
