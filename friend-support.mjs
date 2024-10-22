e4b0c691efae2d69f7b5e9b1bda0692a6311eb08
aelhadda <eaub@proton.me>
2024-10-22 12:39:16 +0100

    test

---
+++ friend-support.mjs
@@ -0,0 +1,44 @@
+//port 5000
+//print a simple message on the console as soon as it starts,
+// status codes: 200, 404 and 500
+//Responses will always be JSON
+
+import http from 'http'
+import { readFile } from 'fs/promises';
+
+
+
+function listenAndServe() {
+    console.log('starting server at port :5000')
+    http.createServer(async (req, res) => {
+        res.setHeader('content-type', 'application/json');
+        const fileName = (req.url).replace('/', '')
+        if (fileName.length == 0) {
+            res.writeHead(404)
+            const obj = { error: 'server failed' }
+            res.write(obj)
+        } else {
+            try {
+                const fileContent = await readFile('./guests/' + fileName + '.json')
+                try {
+                    JSON.parse(fileContent);
+                    res.writeHead(200)
+                    res.write(fileContent)
+                    res.end()
+                } catch (err) {
+                    res.writeHead(500)
+                    const obj = { error: 'server failed' }
+                    res.write(JSON.stringify(obj))
+                    res.end()
+                }
+            } catch (err) {
+                res.writeHead(404)
+                const obj = { error: 'guest not found' }
+                res.write(JSON.stringify(obj))
+                res.end()
+            }
+        }
+    }).listen(5000)
+}
+
+listenAndServe()
