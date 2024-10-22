95abd154b00673eeb1fe446a95f5f21503e947a9
aelhadda <eaub@proton.me>
2024-10-22 17:14:54 +0100

    added auth

---
+++ gatecrashers.mjs
@@ -0,0 +1,35 @@
+import http from 'http'
+import { writeFileSync } from 'fs'
+
+const authenticatedUsers = ['Caleb_Squires:abracadabra',
+    'Tyrique_Dalton:abracadabra',
+    'Rahima_Young:abracadabra'];
+
+http.createServer((req, res) => {
+    let credentials = req.headers.authorization ? req.headers.authorization : '' 
+    credentials = atob(credentials.replace('Basic ', ''))
+    if(!authenticatedUsers.includes(credentials)){
+        res.statusCode = 401;
+        res.end()
+        return
+    }
+    let recivedData = ''
+    req.on('data', dataChunk => {
+        recivedData += dataChunk.toString()
+    })
+    let fileName = 'guests/' + req.url.replace('/', '') + '.json'
+    req.on('end', () => {
+        try {
+            writeFileSync(fileName, recivedData);
+            res.setHeader('content-type', 'application/json')
+            res.statusCode = 201
+            res.end(recivedData)
+        } catch (error) {
+            console.log(error)
+            res.setHeader('content-type', 'application/json')
+            res.statusCode = 500
+            res.end(JSON.stringify({ error: 'server failed' }))
+        }
+    })
+}).listen(5000, 'localhost', () => console.log('starting server at port :5000'));
+
