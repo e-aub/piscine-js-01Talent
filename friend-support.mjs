932c6c3a80bb81ef24ec5b88b588b86592220904
aelhadda <eaub@proton.me>
2024-10-22 14:00:49 +0100

    write head

--- friend-support.mjs
+++ friend-support.mjs
@@ -12,21 +12,21 @@ function listenAndServe() {
     console.log('starting server at port :5000')
     http.createServer((req, res) => {
         let fileName = req.url
+        res.setHeader("content-Type", 'application/json');
         try {
             const fileContent = readFileSync('guests' + fileName + '.json', 'utf-8')
             res.statusCode = 200;
-            res.writeHead("content-Type", 'application/json');
             res.write(fileContent)
             res.end()
         } catch (err) {
             if ((err.code === 'ENOENT')) {
                 res.statusCode = 404;
-                res.writeHead("content-Type", 'application/json');
+                // res.writeHead("content-Type", 'application/json');
                 res.write(JSON.stringify({ error: 'guest not found' }))
                 res.end()
             } else {
                 res.statusCode = 500;
-                res.writeHead("content-Type", 'application/json');
+                // res.writeHead("content-Type", 'application/json');
                 res.write(JSON.stringify({ error: 'server failed' }))
                 res.end()
             }
