63eef73164c3c29f8cf14bb1db81aeeaacbada8d
aelhadda <eaub@proton.me>
2024-10-23 16:36:40 +0100

    gatecrashers.mjs

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -6,27 +6,27 @@ const authenticatedUsers = ['Caleb_Squires:abracadabra',
     'Rahima_Young:abracadabra'];
 
 http.createServer((req, res) => {
+    try {
     let credentials = req.headers.authorization ? req.headers.authorization : ''
     credentials = atob(credentials.replace('Basic ', ''))
     if (!authenticatedUsers.includes(credentials)) {
         res.statusCode = 401;
-        res.end()
+        res.end('Unauthorized')
         return
     }
     let recivedData = req.headers.body
- 
+
     let fileName = 'guests/' + req.url.replace('/', '') + '.json'
     console.log(fileName)
-        try {
-            recivedData = JSON.stringify(JSON.parse(recivedData))
-            writeFileSync(fileName, recivedData);
-            res.setHeader('content-type', 'application/json')
-            res.statusCode = 200
-            res.end(recivedData)
-        } catch (error) {
-            res.setHeader('content-type', 'application/json')
-            res.statusCode = 500
-            res.end(JSON.stringify({ error: 'server failed' }))
-        }
+        recivedData = JSON.stringify(JSON.parse(recivedData))
+        writeFileSync(fileName, recivedData);
+        res.setHeader('content-type', 'application/json')
+        res.statusCode = 200
+        res.end(recivedData)
+    } catch (error) {
+        res.setHeader('content-type', 'application/json')
+        res.statusCode = 500
+        res.end(JSON.stringify({ error: 'server failed' }))
+    }
 }).listen(5000, 'localhost', () => console.log('starting server at port :5000'));
 
